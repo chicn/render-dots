2eedfaea79e58d6c6c7a3be91e0783ccd92938f9
chihiroshimamura <chihiros92@suou.waseda.jp>
2016-12-04 16:46:30 +0530

    Initial Commit

---
+++ render-dots/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  render-dots
+//
+//  Created by Chihiro Shimamura on 12/4/16.
+//  Copyright © 2016 Chihiro Shimamura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
