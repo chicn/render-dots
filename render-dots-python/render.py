7222eaafd4d4442309f54a7f66c50f50ecde42cd
chihiroshimamura <chihiros92@suou.waseda.jp>
2016-12-05 23:36:35 +0530

    Finish rendering part.

--- render-dots-python/render.py
+++ render-dots-python/render.py
@@ -38,61 +38,20 @@ else:
     for i in range(len(filename)):
         # Landmark File の 読み込み
         landmark = open(landmark_dir + landmark_file_name[i])
-        landmark = landmark.read().split("\n")
-        for k in range(len(landmark)):
+        landmark = landmark.read().split("\n")[:-1]
+        for j in range(len(landmark)):
+            # pdb.run('mymodule.test()')
             tmp = [0] * 2
-            tmp[0] = int(landmark[k].split(", ")[0])
-            tmp[1] = int(landmark[k].split(", ")[1])
-            landmark[k] = tmp
+            # tmp[0] = int(landmark[j].split(", ")[0])
+            # tmp[1] = int(landmark[j].split(", ")[1])
+            tmp[0] = int(landmark[j].split(", ")[0])
+            tmp[1] = int(landmark[j].split(", ")[1])
+            landmark[j] = tmp
 
         # render 対象の写真
         img = cv2.imread(indir + filename[i])
         # renderの元になるlocationが入ったデータ => landmark
-        for j in range(len(landmark)):
-            put_text(img, landmark[i][0], landmark[i][1], landmark)
+        for k in range(len(landmark)):
+            put_text(img, landmark[k][0], landmark[k][1], landmark)
         # render 終了
-        cv2.imwrite(outdir + filename[i])
-
-
-
-
-
-# get Landmark
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Lord an color image in grayscale
-# # img = cv2.imread('../images/ibug-300w/indoor_001.png', 0)
-# img = cv2.imread(indir + filename[i])
-
-# # Draw a circle
-# # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
-# # cv2.circle(img, (446, 91), 5, (3,198,2), 1)
-
-# ## prepare location for rendering dots
-# # dot_location = open('/Users/chihiro/Documents/08.Jolie-Joli/Images/ibug/300W/out_indoor/indoor_001.txt').read().split("\n")[0:-1]
-
-
-
-
-
-
-# # Display an image
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.imwrite('../images/ibug-300w-circle/indoor_001_circle.png',img)
-# cv2.destroyAllWindows()
-
-
-#         cv2.putText(img, str(i+1), (dot_location[i][0], dot_location[i][1]), font, 0.3, (3,198,2), 2, cv2.LINE_AA)
+        cv2.imwrite(outdir + filename[i], img)
