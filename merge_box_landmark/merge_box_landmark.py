4e5b3415b64a99b8d98d270171d475b598a1aec3
chihiroshimamura <chihiros92@suou.waseda.jp>
2016-12-13 17:28:14 +0530

    Write guideline of use.

---
+++ merge_box_landmark/merge_box_landmark.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+#
+### How to use ###
+# create a directory like "create_traindata"
+# put those data as below in the same directory.
+# 1. xml file with box data of face.
+# 2. each file of images which contains landmark.
+#
+# Then, you'll find an output of xmldata as traindata.
+#
+###########################################################
+
+
