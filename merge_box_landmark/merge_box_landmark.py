eeb14832caa4f8e83e35dab654797b2dd2367980
chihiroshimamura <chihiros92@suou.waseda.jp>
2016-12-14 19:38:45 +0530

    Add a program to merge landmark file and face box file

--- merge_box_landmark/merge_box_landmark.py
+++ merge_box_landmark/merge_box_landmark.py
@@ -12,4 +12,13 @@
 #
 ###########################################################
 
+import xml.etree.ElementTree as ET
+import numpy as numpy
 
+
+work_dir = "/Users/chihiro/Documents/08.Jolie-Joli/Images/ibug/300W/create_traindata/"
+face_box_file = "output_20161209_100.xml"
+tree = ET.parse(work_dir + face_box_file)  # 返値はElementTree型
+elem = tree.getroot()  # ルート要素を取得(Element型)
+
+print(elem.findtext(".//box"))
