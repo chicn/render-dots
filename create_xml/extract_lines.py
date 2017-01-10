90a24249da6f6d3a192ca941e3547b812c976881
chihiroshimamura <chihiros92@suou.waseda.jp>
2017-01-10 13:57:31 +0900

    Load image file names

--- create_xml/extract_lines.py
+++ create_xml/extract_lines.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import xml.etree.ElementTree as ET
+import glob
 
 # ゴール：single_faceのディレクトリ下のみの学習データ(XMLファイル)を構築する
 
@@ -13,12 +14,39 @@ import xml.etree.ElementTree as ET
 
 # merge
 
-print("Plese input a path to the original train data(xml format).")
+#######################################
+print("Plese input a path to \"the original train data(xml format)\".")
 xml_path = raw_input()
 
-if xml_path[-1] != '/':
-    xml_path += '/'
+if xml_path[-4:] != '.xml':
+    xml_path += '.xml'
 
 tree = ET.parse(xml_path)
 root = tree.getroot()
 
+
+
+#######################################
+print("Please input a path to \"the directory of image data\".")
+img_dir = raw_input()
+# /Users/chihiro/Documents/08.Jolie-Joli/Images/ibug/300W_single_multi/05_Single_Face
+
+if img_dir[-1] != '/':
+    img_dir += '/'
+
+print("Please input a \"TYPE\" of the image files. \n ex) png, jpeg, jpg")
+img_format = raw_input()
+# /Users/chihiro/Documents/08.Jolie-Joli/Images/ibug/300W_single_multi/05_Single_Face
+
+if img_format != 'jpg':
+    if img_format != 'jpeg':
+        if img_format != 'png':
+            print("couldn't recognize the format.")
+
+img_dir += '*.' + img_format
+
+img_names = glob.glob(img_dir)
+
+
+
+#######################################
