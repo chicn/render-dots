ef824199b99cd73af334e6ab9b61dc1970b0e99a
chihiroshimamura <chihiros92@suou.waseda.jp>
2017-01-10 13:40:12 +0900

    Add import xml

--- create_xml/extract_lines.py
+++ create_xml/extract_lines.py
@@ -4,3 +4,21 @@ import numpy as np
 import xml.etree.ElementTree as ET
 
 # ゴール：single_faceのディレクトリ下のみの学習データ(XMLファイル)を構築する
+
+# import xml
+
+# import file name
+
+# extract lines by reffering filenames
+
+# merge
+
+print("Plese input a path to the original train data(xml format).")
+xml_path = raw_input()
+
+if xml_path[-1] != '/':
+    xml_path += '/'
+
+tree = ET.parse(xml_path)
+root = tree.getroot()
+
