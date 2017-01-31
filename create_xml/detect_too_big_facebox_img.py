90a046e16f012916ecf02ce344aa51dc9dbc3c2f
chihiroshimamura <chihiros92@suou.waseda.jp>
2017-01-31 19:24:42 +0530

    Read XML Function

--- create_xml/detect_too_big_facebox_img.py
+++ create_xml/detect_too_big_facebox_img.py
@@ -3,3 +3,12 @@
 import numpy as np
 import cv2
 import xml.etree.ElementTree as ET
+
+### import xml
+xml_path = ""
+while xml_path[-4:] != '.xml':
+    print("[1/4]face_boxの位置情報が入ったXMLファイルへのパスを入力してください。")
+    xml_path = raw_input()
+
+tree = ET.parse(xml_path)
+root = tree.getroot()
