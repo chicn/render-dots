4c4537c253264060e4db92c685599f6c7fe5296d
chihiroshimamura <chihiros92@suou.waseda.jp>
2017-01-31 22:17:16 +0530

    Finish the task

--- create_xml/detect_too_big_facebox_img.py
+++ create_xml/detect_too_big_facebox_img.py
@@ -14,12 +14,28 @@ tree = ET.parse(xml_path)
 root = tree.getroot()
 
 
+total_size, total_height, total_width = 0, 0, 0
+average_size, average_height, average_width = 0, 0, 0
+i = 0
 for image in root.findall('.//image'):
     for box in image.findall('.//box'):
+        img_name = image.attrib['file']
         height = int(box.attrib['height'])
         width = int(box.attrib['width'])
 
-        if height <= 80:
-            if width <= 80:
-                print(image.attrib['file'])
+        print("Name: %s, Boxsize: %d x %d" % (img_name, height, width))
 
+        # if height <= 80:
+        #     if width <= 80:
+        #         print(image.attrib['file'])
+
+        total_size += height * width
+        total_height += height
+        total_width += width
+        i += 1
+
+total_size /= i + 1
+total_height /= i + 1
+total_width /= i + 1
+
+print("average_size: %d, average_height: %d, average_width: %d" % (total_size, total_height, total_width))
