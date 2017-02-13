6ebaf126b4488f20c796d9c0f4de408f436c2dc5
chihiroshimamura <chihiros92@suou.waseda.jp>
2017-02-13 22:08:58 +0530

    Add load_bounding_box_mat function

--- create_xml/create_dlib_xml_traindata.py
+++ create_xml/create_dlib_xml_traindata.py
@@ -10,7 +10,7 @@ from scipy.io import loadmat
 # And convert to be ready for making XML tree
 
 
-def lord_bounding_box_mat(path):
+def load_bounding_box_mat():
     mat_path = ''
     while mat_path[-4:] != '.mat':
         print("face_boxの情報が入った\".mat\"ファイルへのパスを入力してください。")
@@ -18,25 +18,20 @@ def lord_bounding_box_mat(path):
 
         mat_dict = {}
         mat_dict.update(loadmat(mat_path))
+        np_box = mat_dict['bounding_boxes'][0]
+        box_info = []
 
-        box = mat_dict['bounding_boxes']
+        # organizing box_info array
+        for i in np_box:
+            box_info.append([i[0][0][0][0], i[0][0][2].tolist()[0]])
 
-        box_dict = {}
+        # turn numbers into integer
+        for i in range(len(box_info)):
+            for j in range(4):
+                box_info[i][1][j] = int(round(box_info[i][1][j]))
 
-        for i in range(len(box[0])):
-            box_dict.update{box[0][i][0][0][0][0]:
-                            {
-                                'pt1' : box[0][i][0][0][1][0][0]
-                                'pt2' : box[0][i][0][0][1][0][1]
-                                'pt3' : box[0][i][0][0][1][0][2]
-                                'pt4' : box[0][i][0][0][1][0][3]
-                            }
-                           }
-
-
-
-
-
+    return box_info
+     # [u'296814969_3.jpg', [266, 293, 566, 563]]
 
 # Step 2
 # Create base of XML
